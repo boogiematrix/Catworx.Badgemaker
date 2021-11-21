8f8a2617a29c6358d85c627e56e16131c02be2f3
Night Chough <chicken.snuggles@gmail.com>
2021-11-21 14:27:43 -0600

    added employee input

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+// See https://aka.ms/new-console-template for more information
+List<string> employees = new List<string>();
+while (true) 
+{
+    Console.WriteLine("Please enter a name: ");
+// Get a name from the console and assign it to a variable
+    string input = Console.ReadLine();
+    if (input == "")
+    {
+        break;
+    }
+    employees.Add(input);
+}
+for (int i = 0; i < employees.Count; i++)
+{
+    Console.WriteLine(employees[i]);
+}
