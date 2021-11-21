060c74efe1343da6d062b90d0614eee96a408801
Night Chough <chicken.snuggles@gmail.com>
2021-11-21 14:46:12 -0600

    reorganize code

--- Program.cs
+++ Program.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections.Generic;
 // See https://aka.ms/new-console-template for more information
+    // This is our employee-getting code now
+
+static List<string> GetEmployees()
+{
 List<string> employees = new List<string>();
 while (true) 
 {
     Console.WriteLine("Please enter a name: ");
 // Get a name from the console and assign it to a variable
-    string input = Console.ReadLine();
+    string? input = Console.ReadLine();
     if (input == "")
     {
         break;
     }
     employees.Add(input);
 }
-for (int i = 0; i < employees.Count; i++)
+return employees;
+}
+
+static void PrintEmployees(List<string> employees)
 {
-    Console.WriteLine(employees[i]);
+    for (int i = 0; i < employees.Count; i++)
+    {
+        Console.WriteLine(employees[i]);
+    }
+}
+
+{
+    List<string> employees = GetEmployees();
+    PrintEmployees(employees);
 }
